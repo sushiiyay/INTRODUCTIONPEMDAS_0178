6ab9f832f0bc00605de4dc67d146ec11fe872746
sushiiyay <nfa.aliya7811@gmail.com>
2026-02-23 16:57:12 +0800

    first commit

---
+++ IntroductionPemdas_0178.cpp
@@ -0,0 +1,20 @@
+//contoh library
+#include <iostream>
+using namespace std;
+
+//variable global
+string nama; 
+
+int main(){ //program utama
+    int age; //variable local
+    int age1; //variable local
+    age = 20;
+    age1 = 15;
+
+    cout << "Halo World" << endl;
+    cout << 99 << endl;
+    cout << age;
+    cout << age1;
+
+}//selesai
+
